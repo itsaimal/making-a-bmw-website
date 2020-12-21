30e775fb7aa6af01e2d96daf396ef66f656cf32b
itsaimal <itsaimalyousafzai@gmail.com>
2020-12-22 01:20:36 +0500

    progress

--- src/Series.js
+++ src/Series.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import "./Series.css";
+import SeriesList from "./SeriesList"
 
 function Series() {
     return (
         <div className="series">
-            
+            <div className="series__left">
+         <h3>Series</h3>
+            </div>
+
+            <div className="series__right">
+             <h3>Body</h3>
+
+            </div>
+            <SeriesList/>
         </div>
     )
 }
---
+++ src/SeriesList.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import "./SeriesList.css"
+
+function SeriesList() {
+    return (
+        <div className="serieslist">
+            <h2>ALL</h2>
+            <h2>Find your BMW</h2>
+            <h2>1</h2>
+            <h2>2</h2>
+            <h2>3</h2>
+            <h2>4</h2>
+            <h2>5</h2>
+            <h2>6</h2>
+            <h2>7</h2>
+            <h2>8</h2>
+            <h2>Z</h2>
+            <h2>X</h2>
+            <h2>M</h2>
+            <h2>in</h2>
+            <h2>Rechargeable cars</h2>
+            
+        </div>
+    )
+}
+
+export default SeriesList
